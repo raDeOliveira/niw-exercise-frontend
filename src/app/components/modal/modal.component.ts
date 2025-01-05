7ffb3a344b22c931a469b964700fd2700a68ee76
raDeOliveira <oliveiramail.ruben@gmail.com>
2025-01-05 01:10:39 +0000

    added modal

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,29 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+
+  formData: any = {
+    name: '',
+    email: ''
+  };
+
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+  }
+
+  onSubmit(): void {
+    this.dialogRef.close(this.formData);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+}
